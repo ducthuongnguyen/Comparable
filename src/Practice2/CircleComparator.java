951cf5957e156cb86b4504a18e19b789e8daac4c
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-03-29 22:29:52 +0700

    second

---
+++ src/Practice2/CircleComparator.java
@@ -0,0 +1,18 @@
+package Practice2;
+
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
+public class CircleComparator implements Comparator<Circle> {
+    @Override
+    public int compare(Circle o1, Circle o2) {
+        if (o1.getRadius() > o2.getRadius()) return 1;
+        else if (o1.getRadius() < o2.getRadius()) return -1;
+        else return 0;
+    }
+
+
+}
