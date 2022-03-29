6be854a5129f4d25de2d301ad132346abc2cde9b
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-03-29 17:05:58 +0700

    first

---
+++ src/Practice1/Fruit.java
@@ -0,0 +1,5 @@
+package Practice1;
+
+
+public abstract class Fruit implements Edible {
+}
---
+++ src/Practice1/Tiger.java
@@ -0,0 +1,9 @@
+package Practice1;
+
+public class Tiger extends Animal {
+
+    @Override
+    public String makeSound() {
+        return "Tiger: roarrrrr!";
+    }
+}
